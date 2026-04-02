99c45d9d916ac20712857db41967472391884d50
tomnoal123 <tomnoal123@gmail.com>
2026-04-02 22:23:56 +0200

    Update kalkulator.js

--- kalkulator.js
+++ kalkulator.js
@@ -2,20 +2,21 @@ let firstNumber = Number(prompt("Wpisz pierwszą liczbę:"));
 let secondNumber = Number(prompt("Wpisz drugą liczbę"));
 
 function dodawanie(a, b) {
-    console.log(a + b);
+    document.body.innerHTML = "Wynik: " + (a + b);
 }
 
 function odejmowanie(a, b) {
-    console.log(a - b);
+    document.body.innerHTML = "Wynik: " + (a - b);
 }
 
 function mnozenie(a, b) {
-    console.log(a * b);
+    document.body.innerHTML = "Wynik: " + (a * b);
 }
 
 function dzielenie(a, b) {
-    console.log(a / b);
+    document.body.innerHTML = "Wynik: " + (a / b);
 }
+
 
 
 let wynik = prompt("Wybierz działanie, które chcesz wykonać: dodawanie/odejmowanie/mnozenie/dzielenie");
