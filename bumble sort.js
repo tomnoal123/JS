cfd2ec4607f392852c73757cb396cf0cd8253904
tomnoal123 <tomnoal123@gmail.com>
2026-05-01 10:55:03 +0200

    Update bumble sort.js

--- bumble sort.js
+++ bumble sort.js
@@ -1,5 +1,5 @@
 function liczby(tablica){
-    tablica.forEach(() => {
+    
         for (let a = 0; a < tablica.length - 1; a++){
             if(tablica[a] > tablica[a + 1]){
                 let zamiana = tablica[a];
@@ -7,7 +7,7 @@ function liczby(tablica){
                 tablica[a + 1]  = zamiana;
             }
         }
-    })
+    
     return(tablica);
-}
+    }
 console.log(liczby([1, 6, 4, 10, 20]));
